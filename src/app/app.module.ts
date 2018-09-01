3e82153d6594bbca2bac449601491bd8b7f86b2e
ThisuriBandaranayake <thisu1995@gmail.com>
2018-09-01 12:50:16 -0700

    login signuo front end

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 //import { RouterModule} from '@angular/router'
-
+import {MatCardModule} from '@angular/material/card';
 import {MatGridListModule} from '@angular/material/grid-list';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -10,6 +10,17 @@ import { NavComponent } from './nav/nav.component';
 import { FooterComponent } from './footer/footer.component';
 import { LoginComponent } from './login/login.component';
 import { RouterModule} from '@angular/router';
+import {FormsModule } from '@angular/forms';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { FitnessComponent } from './fitness/fitness.component';
+import { GymFitnessComponent } from './gym-fitness/gym-fitness.component';
+import { SwimmingComponent } from './swimming/swimming.component';
+import { SportsComponent } from './sports/sports.component';
+import { RegisterComponent } from './register/register.component';
+import { ArticlesComponent } from './articles/articles.component';
+
+
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -17,16 +28,37 @@ import { RouterModule} from '@angular/router';
     HeaderComponent,
     NavComponent,
     FooterComponent,
-    LoginComponent
+    LoginComponent,
+    SignUpComponent,
+    FitnessComponent,
+    GymFitnessComponent,
+    SwimmingComponent,
+    SportsComponent,
+    RegisterComponent,
+    ArticlesComponent
   ],
   imports: [
     BrowserModule,
     MatGridListModule,
+    FormsModule,
+    MatCardModule,
     RouterModule.forRoot([
      
       {
+        path:'articles',
+        component:ArticlesComponent
+      },
+      {
       path:'login',
       component:LoginComponent
+    },
+      {
+        path:'sign-up',
+        component:SignUpComponent
+      },
+
+     { path:'',
+      component:HomeComponent
       }
     ])
    
---
+++ src/app/gym-fitness/gym-fitness.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gym-fitness',
+  templateUrl: './gym-fitness.component.html',
+  styleUrls: ['./gym-fitness.component.css']
+})
+export class GymFitnessComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
